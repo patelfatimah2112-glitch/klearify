7c33044e768d76e4bd5aa9337884270287806d9c
Fatimah <patelfatimah2112@gmail.com>
2026-05-09 17:25:19 +0530

    Fixed port to 10000 for Render

--- backend/app.py
+++ backend/app.py
@@ -72,5 +72,5 @@ LANGUAGE: {language}
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(
-        os.environ.get('PORT', 5000)), debug=False)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
